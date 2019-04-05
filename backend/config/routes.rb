0ea8b6c6a74094daf19431d330b7e51508b7f87f
Nate Rudnick-Aldridge <nate.rudnickaldridge@gmail.com>
2019-04-05 11:01:53 -0500

    Added some comments (#1)

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # `resources :games` auto-magically creates a bunch of routes for us.
+  # https://guides.rubyonrails.org/routing.html#crud-verbs-and-actions
+  # It basically does all of the CRUD for us in one line.
   resources :games
 end
